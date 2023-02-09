e6227e55b7cf15a0c9280c4fac4cf0076af0eb10
artem-sergiyovych <artem-sergiyovych@users.noreply.github.com>
2023-02-10 00:52:01 +0100

    True folders

---
+++ 2_Code/cmake-build-debug/CMakeFiles/2_Code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_Code"
+  "2_Code.pdb"
+  "CMakeFiles/2_Code.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
