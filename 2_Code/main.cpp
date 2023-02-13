741b52a0f7d9a6d96e5fb896003837e8062af4a7
artem-sergiyovych <artem-sergiyovych@users.noreply.github.com>
2023-02-13 01:15:08 +0100

    first few game items

--- 2_Code/main.cpp
+++ 2_Code/main.cpp
@@ -5,10 +5,14 @@
 
 int main() {
     //Create a window
-    sf::RenderWindow window(sf::VideoMode(2900, 1800), "My window");
+    sf::RenderWindow window(sf::VideoMode(2800, 1600), "My window");
     // Limit the framerate
     sf::Clock clock;
     window.setFramerateLimit(64);
+    /////////////////////////////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SPRITES~~~~~
+    sf::RectangleShape board(sf::Vector2f (1000,1500));
+    board.setFillColor(sf::Color(0,100,80,90));
+    board.setPosition(700,50);
 
     Sprite sprite_back_object("Images/background.png");
     sf::Sprite & sprite_back = sprite_back_object.getSprite();
@@ -16,18 +20,26 @@ int main() {
 
     Sprite sprite_startb_object("Images/start_button.png");
     sf::Sprite & sprite_startb = sprite_startb_object.getSprite();
-    sprite_startb.setPosition(2000,700);
-/*    sf::Texture texture_back;
-    texture_back.loadFromFile("Images/background.png");
-    sf::Sprite sprite_back;
-    sprite_back.setTexture(texture_back);
-    sprite_back.setScale(6,3.5);
-
-    sf::Texture texture_startb;
-    texture_startb.loadFromFile("Images/start_button.png");
-    sf::Sprite sprite_startb;
-    sprite_startb.setTexture(texture_startb);
-    sprite_startb.setPosition(2000,700);*/
+    sprite_startb.setPosition(1900,500);
+    //Legs
+    Sprite sprite_lleg_object ("Images/l_leg.png");
+    sf::Sprite & sprite_lleg = sprite_lleg_object.getSprite();
+    sprite_lleg.setPosition(1050,1200);
+    sprite_lleg.setScale(2,2);
+    Sprite sprite_rleg_object ("Images/r_leg.png");
+    sf::Sprite & sprite_rleg = sprite_rleg_object.getSprite();
+    sprite_rleg.setPosition(1250,1200);
+    sprite_rleg.setScale(2,2);
+    //Leg supports
+    Sprite sprite_llegs_object("Images/l_leg_sup.png");
+    sf::Sprite & sprite_llegs = sprite_llegs_object.getSprite();
+    sprite_llegs.setPosition(955,900);
+    sprite_llegs.setScale(2,2);
+    Sprite sprite_rlegs_object("Images/r_leg_sup.png");
+    sf::Sprite & sprite_rlegs = sprite_rlegs_object.getSprite();
+    sprite_rlegs.setPosition(1340,900);
+    sprite_rlegs.setScale(2,2);
+    /////////////////////////////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SPRITES~End~
 
     while (window.isOpen()) {
         //Event declaration
@@ -47,8 +59,14 @@ int main() {
         }
         // Display the window
         window.clear();
+
         window.draw(sprite_back);
         window.draw(sprite_startb);
+        window.draw(board);
+        window.draw(sprite_llegs);
+        window.draw(sprite_rlegs);
+        window.draw(sprite_lleg);
+        window.draw(sprite_rleg);
         window.display();
     }
 }
