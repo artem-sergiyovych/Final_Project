faea1b5fe05a0972ea0bbddfe5c9b058a77db195
artem-sergiyovych <artem-sergiyovych@users.noreply.github.com>
2023-02-14 15:38:59 +0100

    all sprites within future box and setOrigins
    also first tries of rotations

--- 2_Code/main.cpp
+++ 2_Code/main.cpp
@@ -8,7 +8,7 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(2800, 1600), "My window");
     // Limit the framerate
     sf::Clock clock;
-    window.setFramerateLimit(1);
+    window.setFramerateLimit(60);
     /////////////////////////////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SPRITES~~~~~
     Sprite sprite_back_object("Images/background.png");
     sf::Sprite & sprite_back = sprite_back_object.getSprite();
@@ -33,26 +33,45 @@ int main() {
     //Legs
     Sprite sprite_lleg_object ("Images/l_leg.png");
     sf::Sprite & sprite_lleg = sprite_lleg_object.getSprite();
-    sprite_lleg.setPosition(1050,1200);
+    sprite_lleg.setOrigin(11,9);
+    sprite_lleg.setPosition(1055,1200);
     sprite_lleg.setScale(2,2);
     Sprite sprite_rleg_object ("Images/r_leg.png");
     sf::Sprite & sprite_rleg = sprite_rleg_object.getSprite();
-    sprite_rleg.setPosition(1250,1200);
+    sprite_rleg.setOrigin(46,9);
+    sprite_rleg.setPosition(1360,1200);
     sprite_rleg.setScale(2,2);
     //Leg supports
     Sprite sprite_llegs_object("Images/l_leg_sup.png");
     sf::Sprite & sprite_llegs = sprite_llegs_object.getSprite();
-    sprite_llegs.setPosition(965,900);
+    sprite_llegs.setPosition(955,890);
     sprite_llegs.setScale(2,2);
     Sprite sprite_rlegs_object("Images/r_leg_sup.png");
     sf::Sprite & sprite_rlegs = sprite_rlegs_object.getSprite();
-    sprite_rlegs.setPosition(1330,900);
+    sprite_rlegs.setPosition(1340,890);
     sprite_rlegs.setScale(2,2);
-    //Bumper
+    //Big Blue Bumper
     Sprite sprite_strbumper_object("Images/strong_bumper.png");
     sf::Sprite & sprite_strbumper = sprite_strbumper_object.getSprite();
-    sprite_strbumper.setPosition(1110,600);
+    sf::Vector2f sprite_strbumper_origin (sprite_strbumper.getLocalBounds().width/2, sprite_strbumper.getLocalBounds().height/2);
+    sprite_strbumper.setOrigin(sprite_strbumper_origin);
+    sprite_strbumper.setPosition(1200,700);
     sprite_strbumper.setScale(1.4,1.4);
+    //Small bumpers (4)
+    Sprite sprite_wbumper_object("Images/weak_bumper.png");
+    sf::Sprite & sprite_wbumper1 = sprite_wbumper_object.getSprite();
+    sprite_wbumper1.setOrigin(24,24);
+    sprite_wbumper1.setPosition(1020,670);
+    Sprite sprite_wbumperupper_object("Images/weak_bumper.png");
+    sf::Sprite & sprite_wbumper2 = sprite_wbumperupper_object.getSprite();
+    sprite_wbumper2.setOrigin(24,24);
+    sprite_wbumper2.setPosition(1120,550);
+    sf::Sprite sprite_wbumper3 = sprite_wbumperupper_object.getSprite();
+    sprite_wbumper3.setOrigin(24,24);
+    sprite_wbumper2.setPosition(1280,550);
+    sf::Sprite sprite_wbumper4 = sprite_wbumper_object.getSprite();
+    sprite_wbumper4.setOrigin(24,24);
+    sprite_wbumper1.setPosition(1380,670);
     //Triangles
     Sprite sprite_ltriangle_object("Images/l_triangle.png");
     sf::Sprite & sprite_ltriangle = sprite_ltriangle_object.getSprite();
@@ -79,9 +98,15 @@ int main() {
                 std::cout << "Handling event is closed";
                 exit(EXIT_SUCCESS);
             }
+
         }
+
         // Display the window
         window.clear();
+
+        sprite_lleg.rotate(1.0);
+        sprite_rleg.rotate(-1.0);
+
 
         window.draw(sprite_back);
         window.draw(sprite_startb);
@@ -95,6 +120,10 @@ int main() {
         window.draw(sprite_relbow);
         window.draw(sprite_ltriangle);
         window.draw(sprite_rtriangle);
+        window.draw(sprite_wbumper1);
+        window.draw(sprite_wbumper2);
+        window.draw(sprite_wbumper3);
+        window.draw(sprite_wbumper4);
         window.display();
     }
 }
